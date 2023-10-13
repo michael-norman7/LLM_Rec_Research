57a8790f8300807a756b2b8fed9071f9545b40ed
michael-norman7 <mnorman0801@gmail.com>
2023-10-13 08:53:35 -0500

    added starter code

--- content.js
+++ content.js
@@ -1,3 +1,18 @@
 (() => {
     let youtubeLeftControls, youtubePlayer;
+
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const {type, value, videoId} = obj;
+
+        if (type === "NEW") {
+            currentVideo = videoId;
+            newVideoLoaded();
+        }
+    });
+
+    const newVideoLoaded = () => {
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+
+        console.log(bookmarkBtnExists);
+    }
 })();
