679ae86bcfa7926ad743dbbc9b6056a8c2f235b9
michael-norman7 <mnorman0801@gmail.com>
2023-10-13 18:24:47 -0500

    youtube extension example

--- background.js
+++ background.js
@@ -1,12 +1,11 @@
-chrome.tabs.onUpdate.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-        const queryParameters = tab.url.split("?")[1];
-        const urlParameters = new URLSearchParams(queryParameters);
-        console.log(urlParameters);
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
 
-        chrome.tabs.sendMessage(tabId, {
-            type: "NEW",
-            videoId: urlParameters.get("v")
-        });
-    }
-})
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+    });
+  }
+});
