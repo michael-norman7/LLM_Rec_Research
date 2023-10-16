982aea85ecfba467dd0a2f55294ce67bac4b5bf4
michael-norman7 <mnorman0801@gmail.com>
2023-10-16 11:43:03 -0500

    changed background.js

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-// chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
-//   console.log(info.status);
-//   if (info.url || info.status === "loading") {
-//     const url = info.url || tab.pendingUrl || tab.url;
-//     console.log(url); // prints in the *background* console
-//   }
-// });
+chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
+  console.log(info.status);
+  if (info.url || info.status === "loading") {
+    const url = info.url || tab.pendingUrl || tab.url;
+    console.log(url); // prints in the *background* console
+  }
+});
