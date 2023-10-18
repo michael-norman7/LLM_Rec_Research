f17aea4d7132805490650890c5e737142fd0b134
michael-norman7 <mnorman0801@gmail.com>
2023-10-18 02:33:14 -0500

    working display titles in popup (must open popup)

--- content.js
+++ content.js
@@ -1,7 +1,4 @@
-console.log("run");
-
 const elements = [...document.getElementsByClassName("fallback-text")];
-console.log("load");
 
 elements.forEach((title, idx) => {
   console.log(idx + 1, title.innerHTML);
@@ -9,26 +6,3 @@ elements.forEach((title, idx) => {
 
 const text = elements.map((element) => element.textContent);
 chrome.runtime.sendMessage({ text });
-
-// (async () => {
-//   // TEMP UNTIL WAIT FUNC FIGURED OUT
-//   async function delay(ms) {
-//     return new Promise((resolve) => {
-//       setTimeout(resolve, ms);
-//     });
-//   }
-
-//   document.addEventListener("DOMContentLoaded", () => {
-//     console.log("RUN");
-//   });
-
-//   await delay(5000);
-
-//   let currentTitles = [...document.getElementsByClassName("fallback-text")];
-
-//   // currentTitles.forEach((title, idx) => {
-//   //   console.log(idx + 1, title.innerHTML);
-//   // });
-
-//   chrome.runtime.sendMessage({ currentTitles });
-// })();
--- popup.js
+++ popup.js
@@ -2,7 +2,10 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   const text = message.text;
   const textContainer = document.getElementById("textContainer");
 
-  // Display the retrieved text in the popup
-  textContainer.appendChild()
-  textContainer.textContent = text.join("\n");
+  text.forEach((element) => {
+    let newText = document.createElement("p");
+    newText.textContent = element;
+
+    textContainer.appendChild(newText);
+  });
 });
