b456e3afe275e90931075d881dfc5db74ae36555
michael-norman7 <mnorman0801@gmail.com>
2023-10-19 01:24:39 -0500

    popup lists netflix front page

--- content.js
+++ content.js
@@ -1,8 +1,12 @@
-const elements = [...document.getElementsByClassName("fallback-text")];
+const loadTitles = () => {
+  const elements = [...document.getElementsByClassName("fallback-text")];
 
-elements.forEach((title, idx) => {
-  console.log(idx + 1, title.innerHTML);
-});
+  elements.forEach((title, idx) => {
+    console.log(idx + 1, title.innerHTML);
+  });
 
-const text = elements.map((element) => element.textContent);
-chrome.runtime.sendMessage({ text });
+  const text = elements.map((element) => element.textContent);
+  chrome.runtime.sendMessage({ text });
+};
+
+setTimeout(loadTitles, 2000);
